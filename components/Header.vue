099bdbef1297437e726494ffc21b53b180b2079d
whitenight <emrum01@gmail.com>
2021-11-19 18:46:13 +0900

    feat: タイトル部分用ヘッダーコンポーネントを作成

---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .title
+    h1.title-text タイトル
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  width: 100%;
+  height: 50px;
+  background-color: gray;
+  .title-text {
+    font-size: 30px;
+    text-align: center;
+    margin-top: auto;
+  }
+}
+</style>
